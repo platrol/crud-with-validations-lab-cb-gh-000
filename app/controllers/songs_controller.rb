330a3924f45cd1df29aa18bcab7b24de6115c992
norman lewis <takialog@gmail.com>
2018-03-16 03:08:45 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,37 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+  def show
+    @song = Song.find(params[:id])
+  end
+  def edit
+    @song = Song.find(params[:id])
+  end
+  def create
+    @song = Song.new(post_params(:title, :released, :release_year, :artist_name, :genre))
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(post_params(:title, :released, :release_year, :artist_name, :genre))
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    Song.find(params[:id]).destroy
+        redirect_to songs_path
+
+  end
+  private
+  def post_params(*arg)
+    params.require(:song).permit(*arg)
+
+  end
+end
